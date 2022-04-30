c7c828a8eb04ff271e5b049e8b2ab917b0acb428
Maria Kagermanova <maria.kgm04@gmail.com>
2022-04-30 20:27:19 +0300

    recursion

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,11 +13,74 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.depth = 1;
+    this.temp = 1;
+    this.first = [];
+  }
+  calculateDepth(arr) {
+    if (!Array.isArray(arr)) return;
+    for (let i in arr) {
+      if (i == 0 && this.first.length == 0) this.first.push(arr);
+      if (Array.isArray([arr[i]])) {
+        this.temp++;
+        this.calculateDepth(arr[i]);
+        let j;
+        if (typeof arr[i] == 'object') {
+          j = arr[i].filter(x => {
+            if (Array.isArray(x)) return x;
+          });
+          if (j.length == 0) {
+            if(this.temp > this.depth) {
+              this.depth = this.temp;
+            }
+          }
+        }
+        this.temp--;
+      }
+    }
+    // console.log(arr)
+    let j = this.depth;
+    if (arr == this.first[0]) {
+      this.temp = 1;
+      this.depth = 1;
+      this.first = [];
+    }
+    return j;
+    // let depth = 1;
+    // let temp = 1;
+    // function recur(arr) {
+    //   if (!Array.isArray(arr)) return;
+    //   for (let i in arr) {
+    //     if (Array.isArray([arr[i]])) {
+    //       temp++;
+    //       recur(arr[i]);
+    //       let j;
+    //       if (typeof arr[i] == 'object') {
+    //         j = arr[i].filter(x => {
+    //           if (Array.isArray(x)) return x;
+    //         });
+    //         if (j.length == 0) {
+    //           if(temp > depth) {
+    //             depth = temp;
+    //           }
+    //         }
+    //       }
+    //       temp--;
+    //     }
+    //   }
+    //   return depth;
+    // }
+    // let pretty = recur(arr);
+    // depth = 1;
+    // temp = 1;
+    // return pretty;
   }
 }
+
+
+// const depthCalc = new DepthCalculator();
+// console.log(depthCalc.calculateDepth([1, 2, 3, 4, [1, 2, [1, 2, [[[]]]]], 5, [1, [[[[[[]]]]]]]]));
 
 module.exports = {
   DepthCalculator
