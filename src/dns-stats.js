3ce63403521a1fad62c56eaceb358a2a3380f5f6
Maria Kagermanova <maria.kgm04@gmail.com>
2022-05-01 00:46:47 +0300

    dns

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let obj = {};
+  // for (let i in domains) {
+  //   for (let j = domains[i].length - 1; j >= 0; j--) {
+  //     if (domains[i][j] == '.' || j == 0) {
+  //       if (obj[domains[i].substring(j+1, domains[i].length)] == undefined) {
+  //         obj[domains[i].substring(j, domains[i].length)] = 0;
+  //         for (let k in domains) {
+  //           if (domains[k].includes(domains[i].substring(j, domains[i].length))) obj[domains[i].substring(j, domains[i].length)] += 1;
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+  for (let i in domains) {
+    let st = domains[i].length;
+    let tpAdd = '';
+    for (let j = domains[i].length - 1; j >= 0; j--) {
+      if (domains[i][j - 1] == '.' || j == 0) {
+        tpAdd += '.' + domains[i].substring(j, st);
+        st = j - 1;
+        if (obj[tpAdd] == undefined) obj[tpAdd] = 1;
+        else obj[tpAdd] += 1;
+      }
+    }
+  }
+  return obj
 }
 
 module.exports = {
