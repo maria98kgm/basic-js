a30114668bc38963c36119229db675cb08a98cd5
Maria Kagermanova <maria.kgm04@gmail.com>
2022-04-30 13:54:37 +0300

    vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+// 'AEIHQX SX DLLU!'
+let allLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(reverse) {
+    this.reverse = reverse;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(toTranslate, keyWord) {
+    if (toTranslate == undefined || keyWord == undefined) throw new Error('Incorrect arguments!')
+    let finaleResult = [];
+    for (let i = 0, b = 0; i < toTranslate.length; i++, b++) {
+      if (!toTranslate[i].match(/[a-z]/i)) {
+        finaleResult.push(toTranslate[i]);
+        b--;
+        continue;
+      }
+      if (b == keyWord.length) b = 0;
+      let jey = (allLetters[(allLetters.indexOf(toTranslate[i].toUpperCase()) + allLetters.indexOf(keyWord[b].toUpperCase())) % 26]);
+      finaleResult.push(jey);
+    }
+    return this.reverse == false ? finaleResult.reverse().join('') : finaleResult.join('')
+  }
+  decrypt(toTranslate, keyWord) {
+    if (toTranslate == undefined || keyWord == undefined) throw new Error('Incorrect arguments!')
+    let finaleResult = [];
+    for (let i = 0, b = 0; i < toTranslate.length; i++, b++) {
+      if (!toTranslate[i].match(/[a-z]/i)) {
+        finaleResult.push(toTranslate[i]);
+        b--;
+        continue;
+      }
+      if (b == keyWord.length) b = 0;
+      let jey = allLetters.indexOf(toTranslate[i].toUpperCase()) - allLetters.indexOf(keyWord[b].toUpperCase());
+      if (jey < 0) jey += 26;
+      finaleResult.push(allLetters[jey]);
+    }
+    return this.reverse == false ? finaleResult.reverse().join('') : finaleResult.join('')
   }
 }
 
